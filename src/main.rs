fa63f29cf8c2dc57906aefa5bbfc5fcf12a9f29c
Nathan Anderson <SniperSmiley@gmail.com>
2022-09-02 22:13:30 -0700

    Made sime changes mostly just css an layout

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,37 @@ mod list_component;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-    <body>
+    <body class="main">
         <textarea class="textarea" placeholder="e.g. Hello world" rows="1"></textarea>
-        <div class="box">
-            <button class="button is-large is-success is-family-monospace">{"~"}</button>
-            <div class="field is-grouped">
-                <button class="button is-small is-success is-family-monospace">{"ℤ"}</button>
-                <button class="button is-small is-outlined is-family-monospace">{"\"\""}</button>
-                <button class="button is-small is-outlined is-family-monospace">{"{}"}</button>
-                <button class="button is-small is-outlined is-family-monospace">{"[]"}</button>
-                {"bitwise NOT"}
-            </div>
-            <div class="field is-grouped">
-                <button class="button is-small is-outlined is-family-monospace">{"ℤ"}</button>
-                <button class="button is-small is-success is-family-monospace">{"\"\""}</button>
-                <button class="button is-small is-success is-family-monospace">{"{}"}</button>
-                <button class="button is-small is-outlined is-family-monospace">{"[]"}</button>
-                {"Evaluate"}
-            </div>
-            <div class="field is-grouped">
-                <button class="button is-small is-outlined is-family-monospace">{"ℤ"}</button>
-                <button class="button is-small is-outlined is-family-monospace">{"\"\""}</button>
-                <button class="button is-small is-outlined is-family-monospace">{"{}"}</button>
-                <button class="button is-small is-success is-family-monospace">{"[]"}</button>
-                {"Dump"}
+        <div>
+            <button class="big-button is-success is-family-monospace">{"~"}</button>
+        </div>
+        <div>
+            <div>
+                <div class="field is-grouped">
+                    <button class="button is-option">{"ℤ"}</button>
+                    <button class="button is-blackened">{"\"\""}</button>
+                    <button class="button is-blackened">{"{}"}</button>
+                    <button class="button is-blackened">{"[]"}</button>
+                    {"bitwise NOT"}
+                </div>
+                <div class="field is-grouped">
+                    <button class="button is-blackened">{"ℤ"}</button>
+                    <button class="button is-option">{"\"\""}</button>
+                    <button class="button is-option">{"{}"}</button>
+                    <button class="button is-blackened">{"[]"}</button>
+                    {"Evaluate"}
+                </div>
+                <div class="field is-grouped">
+                    <button class="button is-blackened">{"ℤ"}</button>
+                    <button class="button is-blackened">{"\"\""}</button>
+                    <button class="button is-blackened">{"{}"}</button>
+                    <button class="button is-option">{"[]"}</button>
+                    {"Dump"}
+                </div>
             </div>
         </div>
-
+        <button class="big-button is-option">{"`"}</button>
     </body>
     }
 }
